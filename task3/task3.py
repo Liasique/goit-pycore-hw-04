d34e029d207de21cffaf0ae35fecedb29514a05c
Ollie <olgalukianets7@gmail.com>
2025-06-17 08:17:42 +0200

    Folder is valid

--- task3/task3.py
+++ task3/task3.py
@@ -1,10 +1,25 @@
 import sys
 from pathlib import Path
-from colorama import init, Fore, Style
-init()  # initialize colorama (for terminal)
-print(Fore.YELLOW + "i'm yellow")
-print(Fore.BLUE + "i'm blue")
-print(Style.RESET_ALL + "i'm neitral")
+from colorama import Fore, init
+
+import sys
+from pathlib import Path
+
+if len(sys.argv) < 2:
+    print("Please provide a folder path")
+    sys.exit()
+
+folder = Path(sys.argv[1])
+
+if not folder.exists() or not folder.is_dir():
+    print("Invalid folder path")
+    sys.exit()
+
+print(" Folder is valid:", folder)
+
+#TEST: (base) ➜  goit-pycore-hw-04 git:(main) ✗ python task3/task3.py task2 
+#  Folder is valid: task2
+
 #   i   I sguess that it could be better to create run_task3.sh first
 # 	1.	Create virtual environment -> python -m venv venv
 # 	2.	Activate environment -> source venv/bin/activate
@@ -19,3 +34,20 @@ print(Style.RESET_ALL + "i'm neitral")
 
 
 
+"""
+Розробіть скрипт, який приймає шлях до директорії в якості аргументу командного рядка і візуалізує структуру цієї директорії, виводячи імена всіх піддиректорій та файлів. Для кращого візуального сприйняття, імена директорій та файлів мають відрізнятися за кольором.
+
+
+
+Вимоги до завдання:
+
+Створіть віртуальне оточення Python для ізоляції залежностей проекту.
+Скрипт має отримувати шлях до директорії як аргумент при запуску. Цей шлях вказує, де знаходиться директорія, структуру якої потрібно відобразити.
+Використання бібліотеки colorama для реалізації кольорового виведення.
+Скрипт має коректно відображати як імена директорій, так і імена файлів, використовуючи рекурсивний спосіб обходу директорій (можна, за бажанням, використати не рекурсивний спосіб).
+Повинна бути перевірка та обробка помилок, наприклад, якщо вказаний шлях не існує або він не веде до директорії.
+
+
+
+
+"""
